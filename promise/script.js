e82d3542a4137c76b7581c65322371e0b74ff159
hendraaagil <hendraaagil@gmail.com>
2022-09-17 10:52:05 +0700

    Show data to html

--- promise/script.js
+++ promise/script.js
@@ -38,9 +38,46 @@ const fetchData = async () => {
     console.log(response);
     const resJson = await response.json()
     console.log(resJson);
+    return resJson
   } catch (error) {
     console.error(error)
   }
 }
 
-fetchData()
+const showData = async () => {
+  try {
+    const items = await fetchData()
+
+    // Show data to html
+    // 1. Get the container
+    const container = document.getElementById('container')
+    console.log(container);
+
+    // 2. Mapping data
+    // for (let index = 0; index < items.length; index++) {
+    //   const item = items[index];
+    //   ...
+    // }
+    items.map(item => {
+      console.log(item);
+
+      // 3. Convert data to html element
+      const nameElement = document.createElement('p')
+      nameElement.textContent = item.name
+      const priceElement = document.createElement('p')
+      priceElement.textContent = 'Price: ' + item.price
+
+      const containerItemElement = document.createElement('div')
+      containerItemElement.classList = 'container-item'
+      containerItemElement.appendChild(nameElement)
+      containerItemElement.appendChild(priceElement)
+
+      // 4. Insert element to container
+      container.appendChild(containerItemElement)
+    })
+  } catch (error) {
+    console.error(error)    
+  }
+}
+
+showData()
